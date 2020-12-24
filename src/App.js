9dc6834aae88468e8e5d70f680833f43d1a6a6b3
sagarbuddhic <sagar.buddhi@domain.com.au>
2020-12-24 14:54:32 +1100

    added image, style for list of links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from "./logo.svg";
+
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import { motion } from 'framer-motion';
 import "./App.css";
+import * as styles from './App.style';
+import sagar from './SAGAR_3.jpg';
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.5, when: 'beforeChildren' },
+  },
+};
+
+const variantsChildren = {
+  hidden: { opacity: 0 },
+  visible: (index) => {
+    const list = 3;
+    let divide = index / list;
+    if (divide > 1) {
+      divide -= Math.floor(divide);
+    }
+
+    const delay = 0.4 * (divide * list);
+    return {
+      opacity: 1,
+      transition: {
+        delay,
+      },
+    };
+  },
+};
+
+const name = "SAGAR BUDDHI";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div css={styles.container}>
+      <div css={styles.banner}>
+      <div css={styles.imageWrapper}>
+        <motion.img
+          animate={{x: 50, y: 50, rotate: 360}}
+          transition={{ duration: 2 }}
+          // variants={variants}
+          // drag
+          // dragConstraints={{
+          //   top: -10,
+          //   left: -10,
+          //   right: 100,
+          //   bottom: 50,
+          // }}
+          // dragElastic={1}
+          css={styles.circle}
+          src={sagar}
+          alt="sagar"
         >
-          Learn React
-        </a>
-      </header>
+        </motion.img>
+        <span css={styles.name}>
+        {[...name].map((char, index) => {
+  
+          return (<span
+                  aria-hidden="true"
+                  key={index}
+                  css={styles.nameLetters(index)}
+                >
+                  {char}
+                </span>);
+                })
+        }
+        </span>
+        </div>
+      </div>
+      <div css={styles.content}>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          css={styles.linksContainer}
+        >
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={variantsChildren}
+            css={styles.button}
+            custom="1"
+          >
+            <a href="https://stackoverflow.com/users/2951503/sagar-buddhi" css={styles.links} type="button">Stackoverflow</a>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={variantsChildren}
+            css={styles.button}
+            custom="2"
+          >
+            <a href="https://www.linkedin.com/in/sagar-buddhi-7a187037/" css={styles.links} type="button">Linkedin</a>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={variantsChildren}
+            css={styles.button}
+            custom="3"
+          >
+            <a href="https://github.com/sagarbuddhic" type="button" css={styles.links}>Github</a>
+          </motion.div>
+        </motion.div>
+        <div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/App.style.js
@@ -0,0 +1,93 @@
+
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx, keyframes } from '@emotion/react';
+
+export const container = css`
+    background-color: white;
+`;
+
+export const appHeader = css`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+
+export const content = css`
+    display: grid;
+    grid-auto-flow: row;
+    grid-template-columns: 1fr 2fr;
+`;
+
+export const linksContainer = css`
+    padding: 15px 20px;
+    display: grid;
+    grid-auto-flow: row;
+    grid-gap: 5px;
+`;
+
+export const banner = css`
+    width: 100%;
+    height: 250px;
+    background-color: #3500D3;
+    border-radius: 0px 0px 0px 50px;
+`;
+
+export const button = css`
+    background-color: #f7941d;
+    padding: 10px 0px;
+    box-shadow: 0 0 10px 2px rgba(184,184,184,.5);
+    border-radius: 4px;
+    font-size: 15px;
+    text-decoration: none;
+    text-align: center;
+
+    :hover {
+        background-color: #F1B42F;
+    }
+`;
+
+export const links = css`
+    text-decoration: none;
+    font-weight: bold;
+    color: whitesmoke;
+`;
+
+export const circle = css`
+    background: white;
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+`;
+
+const textAnimate = keyframes`
+  0% { bottom: -0.2em; opacity: 1; }
+  50% { bottom: 0.2em; }
+  100% { bottom: 0; opacity: 1; }
+`;
+
+export const imageWrapper = css`
+    display: grid;
+    grid-gap: 50px;
+    grid-auto-flow: row;
+`;
+
+export const name = css`
+    color: lightcyan;
+    font-size: 30px;
+    font-weight: bold;
+    padding-left: 25px;
+`;
+
+export const nameLetters = (index) => css`
+    animation: ${textAnimate} 0.75s forwards;
+    animation-delay: ${(0.5 + index / 10) + "s"};
+    position: relative;
+    opacity: 0;
+`;
